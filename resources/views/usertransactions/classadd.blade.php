08aaab48f708e0a8a791faea485013e7e9dec3fc
Bruno Rehak <bruno.rehak@gmail.com>
2019-01-14 12:33:10 +0100

    classadd updated with new design

--- resources/views/usertransactions/classadd.blade.php
+++ resources/views/usertransactions/classadd.blade.php
@@ -1,27 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <style>
-        fieldset {
-            width: 90%;
-            margin: auto;
-            margin-top: 10px;
-        }
-    </style>
-    <fieldset>
-        <legend align="left">Dodavanje razreda</legend>
-        <form action="{{ action('UserTransactionController@classadd') }}" method="POST">
-            <input name="_token" type="hidden" value="{{ csrf_token() }}"/>
-            <p>
-                <label>Ime:</label>
-                <input type="text" name="name" id="name" required>
-            </p>
-            <p>
-                <input type="checkbox" name="multi" id="multi">
-                <label>Visestruki unos?</label>
-            </p>
-            <button type="submit" name="submit" id="submit">Unesi</button>
-        </form>
-    </fieldset>
+
+    <h1 id="h1_form_title">Unesi razred</h1>
+    <form action="{{ action('UserTransactionController@classadd') }}" method="POST">
+        <input name="_token" type="hidden" value="{{ csrf_token() }}"/>
+
+        <label for="name" id="form_label">Naziv:</label>
+        <input type="text" name="name" id="generic_input" required>
+
+        <label for="multi" id="form_label">Visestruki unos?</label>
+        <input type="checkbox" name="multi" id="multi"><br>
+
+        <input type="submit" name="submit" id="generic_submit" value="Unesi">
+    </form>
 
 @endsection
