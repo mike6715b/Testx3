e013d1ed6f1407645e1c4eebd45fbedb05ebc5f5
Bruno Rehak <bruno.rehak@gmail.com>
2019-01-14 11:55:31 +0100

    New deign for studadd
    Created a new design for the studadd and added generic input form id's for future use.

--- resources/views/usertransactions/studadd.blade.php
+++ resources/views/usertransactions/studadd.blade.php
@@ -1,47 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-    <style>
-        fieldset {
-            width: 90%;
-            margin: auto;
-            margin-top: 10px;
-        }
-    </style>
-    <fieldset>
+    <div class="studadd">
+        <h1 id="h1_form_title">Dodavanje ucenika</h1>
         <form method="POST" action="{{ action("UserTransactionController@studadd") }}" name="studadd" id="studadd">
             <input name="_token" type="hidden" value="{{ csrf_token() }}"/>
-            <p>
-                <label>Ime Prezime: </label><br>
-                <input type="text" name="name" id="name" required>
-            </p>
-            <p>
-                <label>Username: </label><br>
-                <input type="text" name="uid" id="uid" required>
-            </p>
-            <p>
-                <label>Email: </label><br>
-                <input type="email" name="email" id="email" required>
-            </p>
-            <p>
-                <label>Password: </label><br>
-                <input type="password" minlength="8" name="pwd" id="pwd" required>
-                Leave empty for random *Comming soon*
-            </p>
-            <p>
-                <label>Razred: </label><br>
-                <select name="class" id="class" required>
-                    @foreach(App\Classes::all() as $class)
-                        <option value="{{$class->class_id}}">{{$class->class_name}}</option>
-                    @endforeach
-                </select>
-            </p>
-            <p>
-                <input type="checkbox" name="multi" id="multi">
-                <label>Visestruki unos?</label>
-            </p>
-            <input type="submit" name="submit" id="studSubmit" value="Unesi">
-        </form>
-    </fieldset>
 
+            <label for="name" id="form_label">Ime Prezime: </label>
+            <input type="text" name="name" id="generic_input" required>
+
+            <label for="uid" id="form_label">Korisnicko ime: </label>
+            <input type="text" name="uid" id="generic_input" required>
+
+            <label for="email" id="form_label">Email: </label>
+            <input type="email" name="email" id="generic_input" required>
+
+            <label for="pwd" id="form_label">Lozinka: </label>
+            <input type="password" minlength="8" name="pwd" id="generic_input" required>
+
+            <label for="class" id="form_label">Razred: </label>
+            <select name="class" id="generic_input" required style="width: auto;">
+                @foreach(App\Classes::all() as $class)
+                    <option value="{{$class->class_id}}">{{$class->class_name}}</option>
+                @endforeach
+            </select>
+
+            <input type="checkbox" name="multi" id="multi">
+            <label for="multi" id="form_label">Visestruki unos?</label><br>
+
+            <input type="submit" name="submit" id="generic_submit" value="Unesi">
+        </form>
+    </div>
 @endsection
