525d2d671e3b55f6125461749e92d768907c0faf
Bruno Rehak <bruno.rehak@gmail.com>
2019-01-14 11:54:55 +0100

    Removed admin/student/teahced pages and cleared mainmenu screen

--- resources/views/mainmenu.blade.php
+++ resources/views/mainmenu.blade.php
@@ -1,12 +1,6 @@
 @extends('layouts.app')
 @section('content')
 
-    @if(Auth::user()->user_class == 'admin')
-        @include('mainmenu.admin')
-    @elseif(Auth::user()->user_class == 'teacher')
-        @include('mainmenu.teacher')
-    @else
-        @include('mainmenu.student')
-    @endif
+
 
 @endsection
