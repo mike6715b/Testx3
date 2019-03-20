5b66f97271ea66d2d46781252c15996e161310be
Bruno Rehak <bruno.rehak@gmail.com>
2019-03-20 08:27:45 +0100

    Update subjadd.blade.php

--- resources/views/usertransactions/subjadd.blade.php
+++ resources/views/usertransactions/subjadd.blade.php
@@ -6,11 +6,11 @@
     <form method="POST" id="subjadd" action="{{ action('UserTransactionController@subjadd') }}">
         <input name="_token" type="hidden" value="{{ csrf_token() }}"/>
 
-        <label for="name" id="form_label">Naziv: </label>
+        <label for="name" id="form_label">Naziv predmeta: </label>
         <input type="text" name="name" id="generic_input" required>
 
-        <label for="class" id="form_label">Glavni nastavnik: </label>
-        <select name="class" id="generic_input">
+        <label for="teacher" id="form_label">Glavni nastavnik: </label>
+        <select name="teacher" id="generic_input">
             <option value=""></option>
             @foreach(\App\User::where('user_class', 'teacher')->get() as $value)
                 <option value="{{ $value->user_id }}">{{ $value->user_name }}</option>
