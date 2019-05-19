20f41aa15f692180e39b980e1731bd74adcab6e3
mike6715b <bruno.rehak@gmail.com>
2019-05-19 17:12:59 +0200

    Added Google Analytics
    Added google analytics to track hits

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="{{ asset('css/noty.css') }}">
     <script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
     <script src="{{ asset('js/noty.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('js/gtag.js') }}"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 </head>
